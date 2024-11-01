90b7dbae7099b7edff6c4e37e4ee6738baae7e33
Facunditos <facundolopezcrespo302@gmail.com>
2024-10-31 22:58:56 -0300

    funcion para actualizar reportes estaciones

--- scripts_sql/mysql_db_estaciones.sql
+++ scripts_sql/mysql_db_estaciones.sql
@@ -1,9 +1,9 @@
 /*USE [master]
 GO
 
-GO*/
-CREATE DATABASE estaciones_wunder
 
+CREATE DATABASE estaciones_wunder
+GO*/
 
 
 use estaciones_wunder
@@ -17,13 +17,37 @@ insert into observaciones values
 	(1,'2024-07-01 00:04:00',457800.78,0,0);
 
 select *
-from observaciones
+from observaciones obs
+where id_estacion = 24
+
+select *
+from observaciones obs 
+where (obs.obsTimeLocal  between '2024-10-01 00:00:00' AND '2024-10-15 23:59:59')
+order by obsTimeLocal desc
+
+delete from observaciones
+where (id_estacion =17) 
+
+
 
 select *
 from observaciones obs
 join estaciones est
 on obs.id_estacion = est.id_estacion
-where obs.precipTotal>0 and est.stationID like 'IROSAR18';
+where (obs.id_estacion  = 139)
+order by obs.obsTimeLocal DESC;
+
+select distinct o.id_estacion , sum() o.dia_completo 
+from observaciones o 
+
+select count(*)
+from observaciones obs
+join estaciones est
+on obs.id_estacion = est.id_estacion
+where obs.dia_con_obs =0 ;
+
+insert into observaciones (id_estacion,obsTimeLocal,precipTotal,dia_con_obs,dia_completo)
+values (17,'2024-10-12 00:04:49',0.5,1,1);
 
 
 select datalength(id_observacion) as bytes_id_observacion,datalength(id_estacion) as bytes_id_estacion,datalength(obsTimeLocal) as bytes_obsTimeLocal,datalength(precipTotal) as bytes_precipTotal,datalength(dia_con_obs) as bytes_dia_con_obs,datalength(dia_completo) as bytes_dia_completo
@@ -37,7 +61,7 @@ insert into estaciones values
 	('IPREZ1',-32.997496,-60.768009);
 
 select *
-from estaciones;
+from estaciones e ;
 
 select *
 from estaciones
