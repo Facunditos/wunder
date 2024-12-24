9cb8e2db6eaca088bf99bbcc1110f6e2381d5b17
Facunditos <facundolopezcrespo302@gmail.com>
2024-12-24 11:11:15 -0300

    carga histórico en BD postgres

--- scripts_sql/postgres_db_wunder.sql
+++ scripts_sql/postgres_db_wunder.sql
@@ -5,17 +5,31 @@ CREATE EXTENSION postgis;
 
 drop table estaciones;
 CREATE TABLE estaciones (
-  id_estacion serial primary key,
+  id_estacion int primary key,
   stationID varchar(20) NOT NULL,
   tz varchar(50),
-  ubicacion geometry not null,
+  ubicacion geometry,
   inicio timestamp,
   comentario varchar(50),
   ult_reporte timestamp
 ) 
 
+drop table reportes;
+
 alter table estaciones
 add constraint estacones_ubicacion_point_chk
     check(st_geometrytype(ubicacion) = 'ST_Point'::text);
-    
    
+
+select "stationID" ,"geom",max("obsTimeLocal") as hora ,max("precipTotal_mm") as lluvia_acum
+from estaciones as e
+join reportes as r
+on e.id_estacion = r.id_estacion
+where "obsTimeLocal" between '2024-11-28' and '2024-11-29'
+group by "stationID" ,"geom"
+having ;   
+
+
+select *
+from reportes r 
+where r.fecha =  '2024-01-09' and id_estacion =120;
